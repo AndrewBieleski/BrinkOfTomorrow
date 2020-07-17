8bf2def4c3b89b114578e04df1e87a1a09309dcc
AndrewBieleski <andrewb@prideandjoy.org>
2020-07-18 11:53:04 +1200

    StartingDialogue System

---
+++ Brink of Tomorrow/Assets/Script/Dialogue/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+	String title;
+	String text;
+    Dialogue currentDialogue;
+
+	internal void StartDialogue(Dialogue targetDialogue)
+	{
+        currentDialogue = targetDialogue;
+
+		//currentDialgoue.star
+	}
+
+	internal void LeftButton()
+	{
+
+	}
+
+	internal void RightButton()
+	{
+
+	}
+
+}
