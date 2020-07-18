4bd7a5069fc6550ef70d7e8e264f6cc2e5ceff05
AndrewBieleski <andrewb@prideandjoy.org>
2020-07-18 15:03:25 +1200

    no message

---
+++ Brink of Tomorrow/Assets/Script/Time/TimeClock.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeClock : MonoBehaviour
+{
+    public static float currentTime;
+    public static float maxTime;
+
+    public static float StarTime;
+}
