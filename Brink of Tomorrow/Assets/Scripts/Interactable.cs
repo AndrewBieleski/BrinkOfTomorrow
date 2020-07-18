78f994fa945ba4f33babbcf3910813206e433fe0
Emma Perry <eper472@aucklanduni.ac.nz>
2020-07-18 13:31:08 +1200

    character work

---
+++ Brink of Tomorrow/Assets/Scripts/Interactable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    //variables
+    public bool runThisFrame = false;
+    public bool interactable = false;
+
+    //references
+    public BoxCollider2D bc;
+    public SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact()
+    {
+        Debug.Log("interacted");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.gameObject.CompareTag("Player")) interactable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.gameObject.CompareTag("Player")) interactable = false;
+    }
+}
